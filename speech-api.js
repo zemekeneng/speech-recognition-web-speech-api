b12d30fa58533ff50ac483d7cda4ee8a9eacbb17
Benjamin Ryon <zemekeneng@users.noreply.github.com>
2025-02-10 09:58:32 -0800

    Update speech-api.js

--- speech-api.js
+++ speech-api.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", () => {
     window.SpeechRecognition || window.webkitSpeechRecognition;
   if (typeof SpeechRecognition !== "undefined") {
     const recognition = new SpeechRecognition();
-    recognition.lang = 'es-VE';
+    recognition.lang = 'zh-TW';
     recognition.continuous = true;
     recognition.interimResults = true;
 
